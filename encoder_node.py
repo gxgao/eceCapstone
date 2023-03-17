fd8aa2fb2887503f056eadf6c04594ff4bfa5a35
gxgao <gxgao@andrew.cmu.edu>
2023-03-17 16:49:25 -0400

    encoder publisher infra

---
+++ encoder_node.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# license removed for brevity
+
+import rospy
+from std_msgs.msg import Int16
+
+def rightEncoderPub():
+    pub = rospy.Publisher('leftEncoder', Int16, queue_size=10)
+    rospy.init_node('leftEncoderPublisher', anonymous=True)
+    return pub
+
+def LeftEncoderPub():
+    # topic is right encoder 
+    pub = rospy.Publisher('rightEncoder', Int16, queue_size=10)
+    # node name is right encoder publisher 
+    rospy.init_node('rightEncoderPublisher', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    return pub
+
+
+if __name__ == '__main__':
+
+    rate = rospy.Rate(10) #10hz
+    rpub = rightEncoderPub() 
+    lpub = LeftEncoderPub() 
+
+    while not rospy.is_shutdown():
+        try:
+            rpub.publish(2)
+            lpub.publish(1)
+            rpub.sleep()
+
+            lpub.sleep()
+        except rospy.ROSInterruptException:
+            pass
+        
+    
