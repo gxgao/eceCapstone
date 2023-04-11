1e295ccc6f520b1c055e489661104a8a154c7bf7
gxgao <gxgao@andrew.cmu.edu>
2023-04-11 15:06:11 -0400

    cancel

--- goal_pub.py
+++ goal_pub.py
@@ -3,12 +3,14 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped 
+from actionlib_msgs.msg import GoalID
+
+import argparse
 
 
+cancel_pub = rospy.Publisher("/move_base/cancel", GoalID, queue_size=1)
 goal_2d_pub = rospy.Publisher('goal_2d', PoseStamped, queue_size=0)
 move_base_goal_pub = rospy.Publisher(r'/move_base_simple/goal', PoseStamped, queue_size=0)
-
-
 
 def send_goal(x, y, yaw): 
     rpyPose = PoseStamped() 
@@ -27,13 +29,22 @@ def send_goal(x, y, yaw):
     goal_2d_pub.publish(rpyPose)
     move_base_goal_pub.publish(rpyPose)
 
+
+def cancel_goal():
+    cancel_msg = GoalID()
+    cancel_pub.publish(cancel_msg)
+
 if __name__ == "__main__":
     rospy.init_node('goal_publisher')
     rate = rospy.Rate(10)
+
     while not rospy.is_shutdown(): 
         
+        action = input("x y yaw").split() 
+        if (len(action) == 1):
+            cancel_goal() 
+        else:
+            x, y, yaw = action
+            send_goal(float(x), float(y), float(yaw)) 
 
-        x, y, yaw = input("x y yaw").split() 
-        
-        send_goal(float(x), float(y), float(yaw)) 
         rate.sleep() 
