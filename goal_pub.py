a36d45303bc759598c4782657e1d5efb5d2b39ed
gxgao <gxgao@andrew.cmu.edu>
2023-04-11 14:55:29 -0400

    python changes

--- goal_pub.py
+++ goal_pub.py
@@ -8,6 +8,8 @@ from geometry_msgs.msg import PoseStamped
 goal_2d_pub = rospy.Publisher('goal_2d', PoseStamped, queue_size=0)
 move_base_goal_pub = rospy.Publisher(r'/move_base_simple/goal', PoseStamped, queue_size=0)
 
+
+
 def send_goal(x, y, yaw): 
     rpyPose = PoseStamped() 
     rpyPose.header.frame_id = "map"
@@ -24,3 +26,14 @@ def send_goal(x, y, yaw):
     rpyPose.pose.orientation.w = 0
     goal_2d_pub.publish(rpyPose)
     move_base_goal_pub.publish(rpyPose)
+
+if __name__ == "__main__":
+    rospy.init_node('goal_publisher')
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown(): 
+        
+
+        x, y, yaw = input("x y yaw").split() 
+        
+        send_goal(float(x), float(y), float(yaw)) 
+        rate.sleep() 
