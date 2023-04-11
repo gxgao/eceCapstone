7e592d4b1b70d8e19f4a5a53548aea6a9bcb5331
gxgao <gxgao@andrew.cmu.edu>
2023-04-11 14:41:30 -0400

    python module goal publisher

--- goal_pub.py
+++ goal_pub.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python
+# license removed for brevity
+
+import rospy
+from geometry_msgs.msg import PoseStamped 
 
 
-#  will publish to  /initial_2d and /goal_2d topics.
+goal_2d_pub = rospy.Publisher('goal_2d', PoseStamped, queue_size=0)
+move_base_goal_pub = rospy.Publisher(r'/move_base_simple/goal', PoseStamped, queue_size=0)
+
+def send_goal(x, y, yaw): 
+    rpyPose = PoseStamped() 
+    rpyPose.header.frame_id = "map"
+    rpyPose.header.stamp = rospy.get_rostime()
+    rpyPose.pose.position.x = x
+    rpyPose.pose.position.y = y
+    rpyPose.pose.position.z = 0
+    # double roll, pitch, yaw;
+    # m.getRPY(roll, pitch, yaw);
+    rpyPose.pose.orientation.x = 0
+    rpyPose.pose.orientation.y = 0
+    # yaw in radis
+    rpyPose.pose.orientation.z = yaw
+    rpyPose.pose.orientation.w = 0
+    goal_2d_pub.publish(rpyPose)
+    move_base_goal_pub.publish(rpyPose)
