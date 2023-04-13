e02b24c15ec7429f6accb88c7d4df15a4400a7e8
Serena Ying <sjying@andrew.cmu.edu>
2023-04-13 14:58:46 -0400

    fixed goal publish with actionlib

--- goal_pub.py
+++ goal_pub.py
@@ -5,6 +5,7 @@ import rospy
 from geometry_msgs.msg import PoseStamped 
 from actionlib_msgs.msg import GoalID
 from actionlib_msgs.msg import GoalStatusArray
+from actionlib_msgs.msg import GoalStatus
 from tf.transformations import quaternion_from_euler
 from enum import Enum 
 import video_aruco as va 
@@ -12,6 +13,8 @@ import sys
 import binID
 
 
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 X,Y,Z = 0, 1, 2 
 
@@ -46,10 +49,25 @@ class Robot:
         self.state = ROBOT_STATE.IDLE   
         self.binTracker =  binID.BinTracker() 
 
-
+        self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+        self.client.wait_for_server()
     
     def sendGoal(self, x, y, yaw): 
-        print("Goal sent")
+        # rpyPose = PoseStamped() 
+        # rpyPose.header.frame_id = "map"
+        # rpyPose.header.stamp = rospy.get_rostime()
+        # rpyPose.pose.position.x = x
+        # rpyPose.pose.position.y = y
+        # rpyPose.pose.position.z = 0
+        # # double roll, pitch, yaw;
+        # # m.getRPY(roll, pitch, yaw);
+        # qt = quaternion_from_euler(0, 0, yaw)
+        # rpyPose.pose.orientation.x = qt[0]
+        # rpyPose.pose.orientation.y = qt[1]
+        # # yaw in radis
+        # rpyPose.pose.orientation.z = qt[2]
+        # rpyPose.pose.orientation.w = qt[3]
+
         rpyPose = PoseStamped() 
         rpyPose.header.frame_id = "map"
         rpyPose.header.stamp = rospy.get_rostime()
@@ -64,9 +82,12 @@ class Robot:
         # yaw in radis
         rpyPose.pose.orientation.z = qt[2]
         rpyPose.pose.orientation.w = qt[3]
-        goal_2d_pub.publish(rpyPose)
-        move_base_goal_pub.publish(rpyPose)
 
+        # move_base_goal_pub.publish(rpyPose)
+
+        goal_2d_pub.publish(rpyPose)
+        print("Goal sent")
+        
     def cancelGoal(self):
         cancel_msg = GoalID()
         cancel_pub.publish(cancel_msg)
@@ -82,6 +103,21 @@ class Robot:
                 self.state = ROBOT_STATE.DOCKING 
             elif self.state == ROBOT_STATE.BRINGING_BACK:
                 self.state = ROBOT_STATE.DEDOCKING 
+
+    def movebase_client(self, x, y):
+        x = float(x)
+        y = float(y)
+
+
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = "map"
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = x
+        goal.target_pose.pose.position.y = y
+        goal.target_pose.pose.orientation.w = 1.0
+
+        self.client.send_goal(goal)
+
 
 
 if __name__ == "__main__":
@@ -100,11 +136,12 @@ if __name__ == "__main__":
         
         if (cmd_line is not None and cmd_line == "t"):
             action = input("x y yaw").split() 
-            if (len(action) == 1):
-                rbt.cancelGoal() 
+            if (len(action) <= 1):
+                rbt.client.cancel_goal() 
             else:
                 x, y, yaw = action
-                rbt.sendGoal(float(x), float(y), float(yaw)) 
+                # rbt.sendGoal(float(x), float(y), float(yaw)) 
+                rbt.movebase_client(x, y)
         else:  
         # do some automatic stuff 
             if rbt.state == ROBOT_STATE.IDLE: 
@@ -113,22 +150,25 @@ if __name__ == "__main__":
                 #  [(0, 0, 0.0, 0.0, 0, 0)]      
                 freeBin = rbt.binTracker.getFreeBin() 
                 x, y = freeBin.getXY()
-                print(f"free bin {x}, {y}")
+                print(f"free bin {float(x)}, {float(y)}")
                 rbt.state = ROBOT_STATE.FETCHING 
-                rbt.sendGoal(float(x), float(y), float(0.0))
-            
-            # if rbt.state == ROBOT_STATE.FETCHING: 
-            #     # check video frame once every 10 seconds 
-            #     if cnt % 10 == 0: 
-            #         distAndMarkers = va.fetch_bin_distance_vec()
-            #         if distAndMarkers is not None:
-            #             dist, marker = distAndMarkers 
-            #             if marker[0] == rbt.binTracker.binToGet.arucoId and dist[Z] < 0.8: 
-            #                 # do some movement towards it 
-            #                 print("Cancel Goal")
-            #                 rbt.cancelGoal() 
-            #                 rbt.state = ROBOT_STATE.DOCKING 
-                # goal reach will be done throught hte callback from the subscriber 
+                # rbt.sendGoal(float(x), float(y), float(0.0))
+                rbt.movebase_client(x, y)
+            if rbt.state == ROBOT_STATE.FETCHING: 
+                 # check video frame once every 10 seconds 
+                 if cnt % 10 == 0: 
+                     distAndMarkers = va.fetch_bin_distance_vec()
+                     if distAndMarkers is not None:
+                         dist, marker = distAndMarkers 
+                         print("saw marker")
+                         if marker[0] == rbt.binTracker.binToGet.arucoId and dist[Z] < 0.8: 
+                             # do some movement towards it 
+                             print("Cancel Goal")
+                             rbt.client.cancel_goal() 
+                             rbt.state = ROBOT_STATE.DOCKING 
+                             # goal reach will be done throught hte callback from the subscriber 
+                 if rbt.client.get_state() == GoalStatus.SUCCEEDED:
+                      rbt.state = ROBOT_STATE.SEARCHING_FOR_BIN
             cnt += 1
 
         
